43a5d78146a2f1914a0ec6c7ea91f1abe3d58f3d
David Ollivier <dollivier@exaduo.fr>
2020-11-28 03:40:57 +0100

    date

--- RemoveCustomScript.ps1
+++ RemoveCustomScript.ps1
@@ -1,15 +1,25 @@
 
-$SubscriptionId = Get-AutomationVariable -Name 'subscriptionid'
+# Download files required for this script from github ARMRunbookScripts/static folder
+$FileNames = "msft-wvd-saas-api.zip,msft-wvd-saas-web.zip,AzureModules.zip"
+$SplitFilenames = $FileNames.split(",")
+foreach($Filename in $SplitFilenames){
+Invoke-WebRequest -Uri "$fileURI/ARMRunbookScripts/static/$Filename" -OutFile "C:\$Filename"
+}
+
+#New-Item -Path "C:\msft-wvd-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
+Expand-Archive "C:\AzureModules.zip" -DestinationPath 'C:\Modules\Global' -ErrorAction SilentlyContinue
 
 #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
 $AzCredentialsAsset = 'AzureCredentials'
 $AzCredentials = Get-AutomationPSCredential -Name $AzCredentialsAsset
 $AzCredentials.password.MakeReadOnly()
 
+
 #Authenticate Azure
 #Get the credential with the above name from the Automation Asset store
 Connect-AzAccount -Environment 'AzureCloud' -Credential $AzCredentials
 Connect-AzureAD -AzureEnvironmentName 'AzureCloud' -Credential $AzCredentials
+$SubscriptionId = Get-AutomationVariable -Name 'subscriptionid'
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
 # Install required Az modules
