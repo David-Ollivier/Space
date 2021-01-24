8cd1eee97e5b7fc90bdff7353db0f2353e53f7f2
David Ollivier <david-ollivier@live.fr>
2021-01-24 06:23:41 +0100

    addd

--- Scripts/LaunchSpaceMsix.ps1
+++ Scripts/LaunchSpaceMsix.ps1
@@ -3,6 +3,7 @@ Param(
     $storage,
     $storagepass,
     $sharename,
+    $adminPassword,
     $app1,
     $app2,
     $app3,
@@ -12,6 +13,19 @@ Param(
     $app7,
     $app8 
 )
+
+# $projectname = "space"
+# $storage = "spaceshare0124t040533z.file.core.windows.net" 
+# $storagepass = "83dqltwBa3yXbU3z8IwGErcNl7ouRW6TyEtKqbLMuYLSTV02EQLlYPEQTdBl/tD2wfI0Yne7IJllA/HT38mEZQ==" 
+# $sharename = "msix" 
+# $app1 = "choco install googlechrome" 
+# $app2 = "choco install adobereader" 
+# $app3 = "choco install firefox" 
+# $app4 = "choco install notepadplusplus.install" 
+# $app5 = "choco install python3" 
+# $app6 = "choco install foxitreader" 
+# $app7 = "none" 
+# $app8 = "none"
 
 # Hyper Space
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /NoRestart
@@ -23,8 +37,6 @@ Invoke-WebRequest -Uri $spaceURL -OutFile "c:\space\spaceMsix.ps1"
 
 $TaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass c:\space\spaceMsix.ps1 -projectname $projectname -storage $storage -storagepass $storagepass -sharename $sharename -app1 `"'$app1'`" -app2 `"'$app2'`" -app3 `"'$app3'`" -app4 `"'$app4'`" -app5 `"'$app5'`" -app6 `"'$app6'`" -app7 `"'$app7'`" -app8 `"'$app8'`""
 $TaskTrigger = New-ScheduledTaskTrigger -AtStartup
-$TaskPrincipal = New-ScheduledTaskPrincipal -UserID "spaceMsix\spaceMsix" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -Principal $TaskPrincipal -TaskName "spaceMsix"
-Set-ScheduledTask -TaskName "spaceMsix" -User $TaskPrincipal.UserID -Password 'Tralala123!'
+Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -user "spaceMsix\spaceMsix" -Password $adminPassword -TaskName "spaceMsix" -RunLevel Highest
 
 Restart-Computer -Force
