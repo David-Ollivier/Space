6ecb4614ae7b9bf18353246edb437539f3f8a2e5
David Ollivier <david-ollivier@live.fr>
2021-02-06 04:27:51 +0100

    upup

--- Scripts/LaunchSpaceMsix.ps1
+++ Scripts/LaunchSpaceMsix.ps1
@@ -14,9 +14,44 @@ Param(
 )
 
 Start-Transcript c:\install.log
+        {
+            "type": "Microsoft.Authorization/roleAssignments",
+            "apiVersion": "2018-09-01-preview",
+            "name": "[guid(resourceGroup().id)]",
+            "properties": {
+                "roleDefinitionId": "[concat('/subscriptions/', subscription().subscriptionId, '/providers/Microsoft.Authorization/roleDefinitions/', 'acdd72a7-3385-48ef-bd42-f606fba81ae7')]",
+                "principalId": "<your-principal-id>"
+            }
+        }
+# Mount Sapce Share
+$storageuser = $storage.split('.')[0]
+$storageuser = "Azure\" + $storageuser
+$fullazureshare = '\\' + $storage + '\' + $sharename
+cmd.exe /C "cmdkey /add:$storage /user:$storageuser /pass:$storagepass"
+New-PSDrive -Name Z -PSProvider FileSystem -Root $fullazureshare
 
-# Hyper Space
+
+# Fly Certificate Verification
+new-item -path "c:\space\cert" -ItemType Directory
+New-SelfSignedCertificate -Type Custom -Subject "CN=$projectname" -KeyUsage DigitalSignature -FriendlyName "$projectname" -CertStoreLocation "Cert:\CurrentUser\my" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") -NotAfter (Get-Date).AddMonths(242)
+$password = ConvertTo-SecureString -String space -Force -AsPlainText
+Set-Location Cert:\CurrentUser\my
+$certThmb = ( Get-ChildItem | Where-Object{$_.Subject -eq "CN=$projectname"} ).Thumbprint
+Export-PfxCertificate -cert "Cert:\CurrentUser\my\$certThmb" -FilePath c:\space\cert\cert.pfx -Password $password
+
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("c:\space\cert\cert.pfx","space")
+$rootStore = Get-Item cert:\LocalMachine\Root
+$rootStore.Open("ReadWrite")
+$rootStore.Add($cert)
+$rootStore.Close()
+
+xcopy.exe "c:\space\cert\cert.pfx" $fullazureshare
+
+
+
+# Hyper vSpace Program
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /NoRestart
+
 
 # Getting spaceMsix
 new-item -path "c:\space" -ItemType Directory
@@ -27,7 +62,7 @@ Invoke-WebRequest -Uri $spaceURL -OutFile "c:\space\spaceMsix.ps1"
 # $TaskTrigger = New-ScheduledTaskTrigger -AtStartup
 # Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -user "spaceMsix\spaceMsix" -Password $adminPassword -TaskName "spaceMsix" -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass c:\space\spaceMsix.ps1 -projectname $projectname -storage $storage -storagepass $storagepass -sharename $sharename -app1 `"'$app1'`" -app2 `"'$app2'`" -app3 `"'$app3'`" -app4 `"'$app4'`" -app5 `"'$app5'`" -app6 `"'$app6'`" -app7 `"'$app7'`" -app8 `"'$app8'`""
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass c:\space\spaceMsix.ps1 -projectname $projectname -storage $storage -storagepass $storagepass -sharename $sharename -app1 `"$app1`" -app2 `"$app2`" -app3 `"$app3`" -app4 `"$app4`" -app5 `"$app5`" -app6 `"$app6`" -app7 `"$app7`" -app8 `"$app8`""
 $TaskTrigger = New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -User SYSTEM -Action $action -Trigger $TaskTrigger -TaskName "spaceMsix" -Description "spaceMsix" -Force
 Start-ScheduledTask -TaskName "spaceMsix"
